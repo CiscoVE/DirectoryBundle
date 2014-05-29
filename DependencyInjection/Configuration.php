d2c879e6c3e7ea1e9fe8991bd55417f0bdf230a0
userfriendly <chamalla.extract@gmail.com>
2014-05-29 19:15:21 +0100

    corrected namespace in bundle configuration class

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace WG\LdapBundle\DependencyInjection;
+namespace CiscoSystems\DirectoryBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
